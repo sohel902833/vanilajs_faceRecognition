53029a7c9afa6e6abd0ebf11162b6901725b2396
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2023-03-25 14:43:02 +0600

    face recognition completed

--- script.js
+++ script.js
@@ -10,14 +10,24 @@ async function start() {
   const container = document.createElement("div");
   container.style.position = "relative";
   document.body.append(container);
-  document.body.append("Loaded");
 
+  const labeledFaceDescriptors = await loadLabeledImagesV2();
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+  document.body.append("Loaded");
+  let image;
+  let canvas;
   imageUpload.addEventListener("change", async () => {
-    const image = await faceapi.bufferToImage(imageUpload.files[0]);
+    if (image) {
+      image.remove();
+    }
+    if (canvas) {
+      canvas.remove();
+    }
+    image = await faceapi.bufferToImage(imageUpload.files[0]);
     container.append(image);
-    const canvas = faceapi.createCanvasFromMedia(image);
+    canvas = faceapi.createCanvasFromMedia(image);
     container.append(canvas);
-    const displaySize = { width: image.width, height: image.height };
+    const displaySize = { width: 700, height: 700 };
     faceapi.matchDimensions(canvas, displaySize);
     const detections = await faceapi
       .detectAllFaces(image)
@@ -25,22 +35,50 @@ async function start() {
       .withFaceDescriptors();
     console.log("Detections", detections);
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    resizedDetections.forEach((detection) => {
-      const box = detection.detection.box;
-      const drawBox = new faceapi.draw.DrawBox(box, { label: "Face" });
+    const results = resizedDetections.map((d) =>
+      faceMatcher.findBestMatch(d.descriptor)
+    );
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: result.toString(),
+      });
       drawBox.draw(canvas);
     });
   });
 }
 
-function loadLabeledImages() {
-  const labels = ["Biplob", "Sohel"];
+const loadLabeledImagesV2 = () => {
+  const labels = [
+    {
+      label: "Biplob",
+      images: [
+        "https://i.postimg.cc/j2z4FMKR/1.jpg",
+        "https://i.postimg.cc/4yC1PrD8/2.jpg",
+      ],
+    },
+    {
+      label: "Sohel",
+      images: [
+        "https://i.postimg.cc/Fz7QbJQb/1.jpg",
+        "https://i.postimg.cc/3NVQtJKs/2.jpg",
+      ],
+    },
+  ];
 
   return Promise.all(
     labels.map(async (label) => {
-      for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage("/");
+      const descriptions = [];
+      for (let i of label.images) {
+        const img = await faceapi.fetchImage(i);
+        const detections = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        descriptions.push(detections.descriptor);
       }
+
+      return new faceapi.LabeledFaceDescriptors(label.label, descriptions);
     })
   );
-}
+};
